a74051c8f041c66d4e13cca1e24c656cccdcbe31
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-09-19 04:45:42 -0700

    Python Hello, World

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,8 +1,19 @@
 #include "lists.h"
 
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list - first node of list
+ * Return: 1 if cycle exists, 0 if it does not
+ */
+
 int check_cycle(listint_t *list)
 {
 	listint_t *tmp,*first;
+
+	if(!list)
+	{
+		return (NULL);
+	}
 
 	first = list;
 	tmp = list;
