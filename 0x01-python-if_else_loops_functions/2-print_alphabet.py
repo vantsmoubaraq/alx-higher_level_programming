d83e4316d2b8592e5917ffed507c3909e33502f1
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-09-20 02:44:29 -0700

    Python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for letter in range(ord('a'),(ord('z')+1)):
-    print("{}".format(letter),end = '')
+for letter in range(ord('a'), ord('z')+1):
+    print("{:s}".format(letter), end='')
