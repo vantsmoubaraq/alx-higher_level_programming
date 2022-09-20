3a9e2def3751548be29dbd4704c410e4ef4a3827
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-09-20 03:51:55 -0700

    Python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-for i in range(0,8):
-    for j in range(1,10):
-        print("{:d}{:d}".format(i,j), end=', ')
+for i in range(0, 8):
+    for j in range(1, 10):
+        print("{:d}{:d}".format(i, j), end=', ')
 print("{:d}".format(89))
-
