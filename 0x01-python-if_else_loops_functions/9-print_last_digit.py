c0c3e90ed621d4e7ad5ec3b4f01edbd345cee10e
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-09-20 04:21:12 -0700

    Python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,9 +3,8 @@
 def print_last_digit(number):
     if(number >= 0):
         remainder = number % 10
-        print(f"{remainder}",end = '')
+        print(f"{remainder}", end='')
     else:
         remainder = -(number % -10)
-        print(f"{remainder}",end = '') 
+        print(f"{remainder}", end='')
     return (remainder)
-
