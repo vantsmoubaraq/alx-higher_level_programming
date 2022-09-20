54f5ed49fd921f669ed1a1728a983752db24f23e
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-09-20 04:04:21 -0700

    Python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for letter in range(ord('a'),ord('z')):
+    for letter in range(ord('a'), ord('z')):
         if(letter == ord(c)):
             return True
     return False
-
