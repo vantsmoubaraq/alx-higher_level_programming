f2bb859efa36e5970765fc78f97a4c7594584643
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-09-22 02:27:02 -0700

    Python-import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     from sys import argv
-    from calculator_1 import add, sub, mul, div
-
-    le = len(argv)
-    a = argv[1]
-    b = argv[3]
-
-    if(le < 3):
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         quit(1)
-
-    if(argv[2] == '+'):
-        print("{:s} + {:s} = {:d}".format(a, b, add(int(a), int(b))))
-    elif(argv[2] == '-'):
-        print("{:s} - {:s} = {:d}".format(a, b, sub(int(a), int(b))))
-    elif(argv[2] == "*"):
-        print("{:s} * {:s} = {:d}".format(a, b, mul(int(a), int(b))))
-    elif(argv[2] == '/'):
-        print("{:s} / {:s} = {:d}".format(a, b, div(int(a), int(b))))
+    a = int(argv[1])
+    b = int(argv[3])
+    ops = ["+", "-", "*", "/"]
+    from calculator_1 import add, sub, mul, div
+    funcs = [add, sub, mul, div]
+    for i, s in enumerate(ops):
+        if argv[2] == s:
+            print("{} {} {} = {}".format(a, s, b, funcs[i](a, b)))
+            break
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         quit(1)
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-
-from sys import argv
-
-sum = 0
-
-for i in range(len(argv)):
-    if(i >= 1):
-        sum += int(argv[i])
-
-print("{:d}".format(sum))
+if __name__ == "__main__":
+    from sys import argv
+    add = 0
+    for s in argv[1:]:
+        add += int(s)
+    print("{:d}".format(add))
