9bead4b5cb99b3bde702aea4b190f306ae8639d5
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-09-28 02:32:54 -0700

    Python-more_data_structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,4 +3,4 @@
 def search_replace(my_list, search, replace):
     def replace_it(element):
         return (element if element != search else replace)
-    return(list(map(replace_it,my_list)))
+    return(list(map(replace_it, my_list)))
