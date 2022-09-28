b4eddd5627560e0aa11e4693880b55b8c49c2169
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-09-28 04:41:10 -0700

    Python-more_data_structures

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    sum = 0
-    weights = 0
+    if my_list:
+        sum = 0
+        weights = 0
 
-    for tuple in my_list:
-        sum += (tuple[0] * tuple[1])
+        for tuple in my_list:
+            sum += (tuple[0] * tuple[1])
 
-    for element in my_list:
-        weights += element[1]
+        for element in my_list:
+            weights += element[1]
 
-    return (sum/weights)
+        return (sum/weights)
+    return None
