5ffc74e2b7b00b740e61d6f2f6a40769e897c085
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-09-28 02:39:07 -0700

    Python-more_data_structures

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-ef simple_delete(a_dictionary, key=""):
+def simple_delete(a_dictionary, key=""):
     if key in a_dictionary:
         del a_dictionary[key]
         return a_dictionary
