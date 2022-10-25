7a2e07b1e05b75cb09425a4ec385e2a71015552b
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-10-25 04:50:42 -0700

    Python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,5 +2,5 @@
 
 def read_file(filename=""):
     """reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, 'r', encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
