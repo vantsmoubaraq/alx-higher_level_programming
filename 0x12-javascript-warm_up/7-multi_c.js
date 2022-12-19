cd7df0483bc37e9863442dde84b3b045a15fb7fe
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-12-19 01:50:10 -0800

    javascript-warm_up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const args = process.argv
+const args = process.argv;
 
 const num = parseInt(args[2]);
 let i;
@@ -9,6 +9,6 @@ if (isNaN(num)) {
   console.log('Missing number of occurrences');
 } else {
   for (i = 0; i < num; i++) {
-  console.log('C is fun');
-}
+    console.log('C is fun');
+  }
 }
