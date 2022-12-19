029fd61ff31caa229dd54546c52dc7ff7329cd03
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-12-19 01:48:19 -0800

    javascript-warm_up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const args = process.argv
+const args = process.argv;
 
 if (parseInt(args[2])) {
-  console.log("My number: " + parseInt(args[2]));
+  console.log('My number: ' + parseInt(args[2]));
 } else {
-  console.log("Not a number");
+  console.log('Not a number');
 }
