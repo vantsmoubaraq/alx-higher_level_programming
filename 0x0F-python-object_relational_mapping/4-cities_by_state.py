330563fb644da412942b9dedc39927c20e59b231
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-12-20 03:29:24 -0800

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
-'''
+"""
 Module  lists all cities from the database hbtn_0e_4_usa
-'''
+"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    '''
+    """
     Not be executed when imported
-    '''
+    """
     user = argv[1]
     password = argv[2]
     db = argv[3]
@@ -19,12 +19,14 @@ if __name__ == "__main__":
                            passwd=password, db=db, charset="utf8")
     session = conn.cursor()
 
-    session.execute('SELECT * FROM cities ORDER BY cities.id')
+    session.execute("""SELECT cities.id, cities.name, states.name FROM cities
+                    INNER JOIN states on states.id = cities.state_id ORDER BY
+                    cities.id""")
 
-    states = session.fetchall()
+    results = session.fetchall()
 
-    for state in states:
-        print(state)
+    for result in results:
+        print(result)
 
     session.close()
     conn.close()
