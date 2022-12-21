6c1f838e9ab86b7fc268df3f124890136d090fe8
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-12-21 02:43:46 -0800

    object_relational_mapping

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 
+"""
+deletes all State objects with a name containing the letter a from the database
+"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3]))
+    """
+    Not to be executed
+    """
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+                           argv[1], argv[2], argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
 
--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,5 +1,8 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 
+"""
+model_city.py that contains the class definition of a City
+"""
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_city import City
@@ -7,12 +10,17 @@ from model_state import Base, State
 from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3]))
+    """
+    Not to be executed when imported
+    """
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+                           argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    results = session.query(City, State).join(State, State.id==City.state_id).order_by(City.id).all()
-    
+    results = session.query(City, State).join(State, State.id == City.sta
+                                              te_id).order_by(City.id).all()
+
     for city, state in results:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
