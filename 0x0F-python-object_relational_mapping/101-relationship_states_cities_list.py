8ee171c06b09e0e4a7e6610db03cf11411929a22
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-12-22 15:26:26 +0300

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
+""" prints the State object with the name passed as argument from the database
 """
-list all State objects, and corresponding City objects,
-contained in the database
-"""
-from sys import argv
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import Session
-from relationship_city import City
+import sys
 from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
+
 
 if __name__ == "__main__":
-    username = argv[1]
-    password = argv[2]
-    database = argv[3]
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                           username,
-                           password,
-                           database),
-                           pool_pre_ping=True
-                           )
-    session = Session(engine)
-
-    all_states = session.query(State).order_by(State.id).all()
-    for state in all_states:
-        print("{}: {}".format(state.id, state.name))
-        for city in state.cities:
-            print("{}{}: {}".format(" "*4, city.id, city.name))
-
-    session.close()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
+        for city_ins in instance.cities:
+            print("    ", end="")
+            print(city_ins.id, city_ins.name, sep=": ")
--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
 """
-class definition of city, similar to state
+Contains the class definition of a City
 """
-from sqlalchemy import Column, Integer, String, ForeignKey
 from relationship_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class City(Base):
-    """ City class """
+    """
+    Class that defines each city
+    """
     __tablename__ = 'cities'
-    id = Column(
-                Integer,
-                autoincrement=True,
-                unique=True,
-                nullable=False,
-                primary_key=True
-                )
-    name = Column(
-                  String(128),
-                  nullable=False
-                  )
-    state_id = Column(
-                      Integer,
-                      ForeignKey("states.id"),
-                      nullable=False,
-                      )
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
