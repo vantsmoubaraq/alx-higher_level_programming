58199b5f4fa4cb2398e3530941274f9316bafeac
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-12-22 12:15:06 +0300

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
+""" prints the State object with the name passed as argument from the database
 """
-lists all City objects from the database
-"""
-from sys import argv
+import sys
+from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy import (create_engine)
-from sqlalchemy.orm import Session
-from relationship_city import Base, City
-from relationship_state import State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
+
+
 if __name__ == "__main__":
-    username = argv[1]
-    password = argv[2]
-    database = argv[3]
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                           username,
-                           password,
-                           database),
-                           pool_pre_ping=True
-                           )
-    session = Session(engine)
-
-    cities = session.query(City).order_by(City.id).all()
-
-    for city in cities:
-        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
-    session.close()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        for city_ins in instance.cities:
+            print(city_ins.id, city_ins.name, sep=": ", end="")
+            print(" -> " + instance.name)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 """
-State class
+Contains State class and Base, an instance of declarative_base()
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship
 
-Base = declarative_base()
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
 
 
 class State(Base):
     """
-    State class
+    Class with id and name attributes of each state
     """
     __tablename__ = 'states'
-
-    id = Column(Integer, autoincrement=True, unique=True,
-                nullable=False, primary_key=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City",
-                          backref="state",
-                          cascade="all, delete-orphan")
+    cities = relationship("City", backref="states")
