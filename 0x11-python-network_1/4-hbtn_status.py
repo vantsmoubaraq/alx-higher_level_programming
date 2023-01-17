88e42318c44d067adbdcf2b5b09debe28f054ce1
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-01-17 14:22:51 +0300

    python-network_1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,5 +8,5 @@ import requests
 if __name__ == "__main__":
     response = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(response)))
+    print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
