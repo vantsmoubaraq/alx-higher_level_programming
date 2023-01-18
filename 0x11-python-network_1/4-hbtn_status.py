e6c63526424b6bdef7f05950c37b5be0803fe823
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-01-18 03:20:35 +0300

    python_network_1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""
-    Fetches https://intranet.hbtn.io/status
-    using package 'requests'
-"""
-
+"""Script that fetches https://alx-intranet.hbtn.io/status."""
 import requests
 
+
 if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}\n\t- content: {}"
-          .format(type(r.text), r.text))
+    req = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
